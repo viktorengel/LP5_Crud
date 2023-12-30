d0222389cf091e0148506ce3afb462817c3e269c
Viktor Rengel <viktorengel@hotmail.com>
2023-12-29 23:51:13 -0500

    Proyecto limpio para emprezar hacer el crud

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -6,9 +6,5 @@ use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    public function index($id = 2024)
-    {
-        /* $note = Note::find($id); */
-        return view('note.index', compact('id'));
-    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,4 @@
 use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-/* Route::get('/', [Controller::class, 'funtion'])->name('example'); */
-/* Route::get('/note/{id}/{title}/{slug}', [NoteController::class, 'index'])->name('note.index'); */
-/* Route::get('/note/80', [NoteController::class, 'example'])->name('note.example'); */
-Route::get('/note/{id}', [NoteController::class, 'index'])->name('note.index');
